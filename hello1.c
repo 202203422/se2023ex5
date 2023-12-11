f47d46bcb1d319646c5a72f627f7c513304068de
takahashi <r202203422ne@jindai.jp>
2023-12-11 14:50:57 +0900

    using for

--- hello1.c
+++ hello1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(void) { 
-    char str[] = "hello";
-    printf("%s\n", str);
+int main(void) {
+    int x = 5; 
+
+    for (int i = 0; i < x; i++) {
+        printf("hello\n");
+    }
+
     return 0;
 }
